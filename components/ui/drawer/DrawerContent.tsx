1f148b6883cd563fd195da1bfc562f7cf2714395
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2024-11-05 12:23:48 +0400

    feat-2-routing: remove unnecessary wrapper

--- components/ui/drawer/DrawerContent.tsx
+++ components/ui/drawer/DrawerContent.tsx
@@ -1,6 +1,7 @@
 import { DrawerItems } from "@/components/ui/drawer/DrawerItems";
 import { MenuIcon } from "@/components/ui/menu-icon";
 import { Text } from "@/components/ui/text";
+import { ICON_SIZE } from "@/constants";
 import { useUserStore } from "@/store/userStore";
 import { Roles } from "@/types";
 
@@ -8,7 +9,7 @@ import {
   DrawerContentComponentProps,
   DrawerContentScrollView,
 } from "@react-navigation/drawer";
-import { Image, Pressable, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
 
 import { useRouter } from "expo-router";
 
@@ -24,34 +25,42 @@ export function DrawerContent(props: DrawerContentComponentProps) {
   };
 
   return (
-    <View className="flex-1">
-      <DrawerContentScrollView {...props}>
-        <View className="border-secondary-100 border-b p-4 flex flex-row justify-between items-center">
-          <View className="flex flex-row items-center gap-4">
-            <Image
-              source={require("@/assets/images/avatar.png")}
-              className="w-12 h-12 border border-secondary-100 aspect-square"
-              resizeMode={"contain"}
-            />
-            <View>
-              <Text className="text-brand-600 gap-1" variant="h6">
-                User Name
+    <DrawerContentScrollView {...props} style={styles.container}>
+      <View className="border-secondary-100 border-b p-4 flex flex-row justify-between items-center">
+        <View className="flex flex-row items-center gap-4">
+          <Image
+            source={require("@/assets/images/avatar.png")}
+            className="w-12 h-12 border border-secondary-100 aspect-square"
+            resizeMode={"contain"}
+          />
+          <View>
+            <Text className="text-brand-600 gap-1" variant="h6">
+              User Name
+            </Text>
+            <View className="border-t-[1px] border-secondary-100">
+              <Text className="text-secondary-500" variant="textMediumSmall">
+                {Roles.ADMIN.toLocaleUpperCase()}
               </Text>
-              <View className="border-t-[1px] border-secondary-100">
-                <Text className="text-secondary-500" variant="textMediumSmall">
-                  {Roles.ADMIN.toLocaleUpperCase()}
-                </Text>
-              </View>
             </View>
           </View>
-          <Pressable className="ml-3" onPress={logOut}>
-            {({ pressed }) => (
-              <MenuIcon active={pressed} size={24} IconComponent={LogoutIcon} />
-            )}
-          </Pressable>
         </View>
-        <DrawerItems state={props.state} descriptors={props.descriptors} />
-      </DrawerContentScrollView>
-    </View>
+        <Pressable className="ml-3" onPress={logOut}>
+          {({ pressed }) => (
+            <MenuIcon
+              active={pressed}
+              size={ICON_SIZE}
+              IconComponent={LogoutIcon}
+            />
+          )}
+        </Pressable>
+      </View>
+      <DrawerItems state={props.state} descriptors={props.descriptors} />
+    </DrawerContentScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 26,
+  },
+});
